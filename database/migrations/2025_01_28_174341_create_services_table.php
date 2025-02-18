fcf3ea1fbec54cc664dd0f0c147db1b3ffca94a4
Mahdy Adel Mahdy <101920007+mahdyadel00@users.noreply.github.com>
2025-02-18 10:30:20 +0200

    Update 2025_01_28_174341_create_services_table.php

--- database/migrations/2025_01_28_174341_create_services_table.php
+++ database/migrations/2025_01_28_174341_create_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->decimal('price');
-            $table->boolean('status')->default(true);
+//            $table->boolean('status')->default(true);
             $table->decimal('vat')->default(0);
             $table->timestamps();
         });
