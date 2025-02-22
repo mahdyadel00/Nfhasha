9d2b294948e6afdcbfac7628868fc19beb147e5f
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2025-02-22 23:31:16 +0200

    عحيشفث

--- app/Models/ServiceOfferTranslation.php
+++ app/Models/ServiceOfferTranslation.php
@@ -2,20 +2,17 @@
 
 namespace App\Models;
 
-use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceOfferTranslation extends Model
 {
-    use HasFactory , Translatable;
-    public $translatedAttributes = ['name' , 'description'];
-
+    use HasFactory;
 
     protected $fillable = [
         'service_offer_id',
         'locale',
-        'title',
+        'name',
         'description',
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('splash-screens' , [SplashScreensController::class, 'index']);
 //get service_offers
 Route::get('service-offers' , [ServiceOfferController::class , 'index']);
+Route::get('service-offer/{id}' , [ServiceOfferController::class , 'show']);
+//get copun
+Route::post('coupon/{id}' , [ServiceOfferController::class , 'coupon']);
 Route::get('home-sliders' , HomeSlidersController::class);
 Route::get('services' , ServicesController::class);
 
