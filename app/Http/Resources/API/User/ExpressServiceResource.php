59c484972ae6937305d651e0b519afac43e41285
Mahdy Adel Mahdy <mahdyadel00@gmail.com>
2025-03-08 01:45:45 +0200

    Update ExpressServiceResource.php

--- app/Http/Resources/API/User/ExpressServiceResource.php
+++ app/Http/Resources/API/User/ExpressServiceResource.php
@@ -22,7 +22,7 @@ class ExpressServiceResource extends JsonResource
             return $this->$relation()
                 ->where('user_id', $request->user()->id)
                 ->where('express_service_id', $this->id)
-                ->where('order_id', $request->order_id) // ✅ إضافة شرط `order_id`
+                ->where('order_id', $this->order_id) // ✅ إضافة شرط `order_id`
                 ->first();
         };
 
